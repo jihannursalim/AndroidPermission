595f802e6451cde668791c6cbba0663a36c00cff
nisanurainisalim <ainisyarifsuhandari@gmail.com>
2023-04-18 00:19:33 +0800

    Latihan 4

--- app/src/main/java/com/example/androidpermission/Dangerous/DangerousPermissionLat2.kt
+++ app/src/main/java/com/example/androidpermission/Dangerous/DangerousPermissionLat2.kt
@@ -5,16 +5,21 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.example.androidpermission.R
 import com.example.androidpermission.databinding.ActivityDangerousPermissionLat2Binding
+import java.io.File
 
 class DangerousPermissionLat2 : AppCompatActivity() {
 
@@ -25,49 +30,128 @@ class DangerousPermissionLat2 : AppCompatActivity() {
         binding = ActivityDangerousPermissionLat2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        checkCameraPermission()
+//        checkCameraPermission()
 
-        val myButton = findViewById<Button>(R.id.btnTakePhoto)
-        myButton.setOnClickListener {
-            // Aksi yang ingin dilakukan ketika tombol diklik
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        val REQUEST_CODE_CAMERA = 1
+        val REQUEST_CODE_GALLERY = 2
+
+
+        fun openCamera() {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, REQUEST_CODE_CAMERA)
         }
 
+        fun openGallery() {
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            galleryIntent.type = "image/*"
+            startActivityForResult(galleryIntent, REQUEST_CODE_GALLERY)
+        }
 
-    }
+        fun checkPermission() {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ),
+                    101
+                )
+            } else {
+                // Permission already granted
+                // Do something
+            }
+        }
 
-    private val REQUEST_CODE_CAMERA = 1
-
-
-
-    private fun checkCameraPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
+        fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
         ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.CAMERA),
-                REQUEST_CODE_CAMERA
-            )
-        } else {
-            // Permission already granted
-            openCamera()
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            if (requestCode == 101) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    // Do something
+                }
+            }
         }
-    }
 
-    private fun openCamera() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, REQUEST_CODE_CAMERA)
-    }
+        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            var imageView = findViewById<ImageView>(R.id.viewImage)
+            if (resultCode == RESULT_OK) {
+                when (requestCode) {
+                    REQUEST_CODE_CAMERA -> {
+                        val imageBitmap = data?.extras?.get("data") as Bitmap
+                        imageView.setImageBitmap(imageBitmap)
+                    }
+                    REQUEST_CODE_GALLERY -> {
+                        val selectedImageUri = data?.data
+                        imageView.setImageURI(selectedImageUri)
+                    }
+                }
+            }
+        }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == REQUEST_CODE_CAMERA && resultCode == Activity.RESULT_OK) {
-            val image = data?.extras?.get("data") as Bitmap
-            // Do something with the image
+        binding.btnTakePhoto.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                checkPermission()
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.CAMERA
+                    ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    openCamera()
+                }
+            } else {
+                openCamera()
+            }
+        }
+
+        binding.btnChoosePhoto.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                checkPermission()
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.CAMERA
+                    ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    openGallery()
+                }
+            } else {
+            }
         }
     }
 }
